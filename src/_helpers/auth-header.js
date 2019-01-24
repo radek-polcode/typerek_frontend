b93c8b7e0f2d273b5a71bda3dfcb73e8ce4557a4
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2019-01-24 11:59:50 +0100

    Add auth headers to user item on local storage

--- src/_helpers/auth-header.js
+++ src/_helpers/auth-header.js
@@ -1,8 +1,17 @@
+import config from '../_config/'
+
 export function authHeader() {
   let user = JSON.parse(localStorage.getItem('user'));
 
-  if (user && user.token) {
-    return { 'Authorization': 'Bearer ' + user.token }; 
+  if (user && user.authData) {
+    const authData = user.authData
+    return Object.assign({
+      'access-token': authData['access-token'],
+      'client': authData['client'],
+      'expiry': authData['expiry'],
+      'token-type': authData['token-type'],
+      'uid': authData['uid']
+    }, config.defaultHeaders); 
   } else {
     return {};
   }
--- src/_services/user.service.js
+++ src/_services/user.service.js
@@ -83,12 +83,16 @@ function _delete(id) {
 
 function handleResponse(response) {
   return response.text().then(text => {
-      const data = text && JSON.parse(text);
+      let data = text && JSON.parse(text);
+      if (data && response.headers.get('access-token')) {
+        data = assignAuthHeaders(data, response.headers)
+      }
+      console.log(data)
       if (!response.ok) {
           if (response.status === 401) {
               // auto logout if 401 response returned from api
               logout();
-              window.location.reload(true);
+              // window.location.reload(true);
           }
 
           const error = (data && data.message) || response.statusText;
@@ -97,4 +101,17 @@ function handleResponse(response) {
 
       return data;
   });
+}
+
+function assignAuthHeaders(data, responseHeaders) {
+  return Object.assign(
+          { 
+            'authData': {
+              'access-token': responseHeaders.get('access-token'),
+              'client': responseHeaders.get('client'),
+              'expiry': responseHeaders.get('expiry'),
+              'token-type': responseHeaders.get('token-type'),
+              'uid': responseHeaders.get('uid')
+            }
+          }, data)
 }
