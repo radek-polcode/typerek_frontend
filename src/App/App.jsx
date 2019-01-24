a0609fd6d22ef4f35bee6a497344a183848eea75
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2019-01-24 14:46:36 +0100

    Update App component, remove unnecessary css

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Router, Route } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { Container } from 'reactstrap'
 
 import { history } from '../_helpers'
 import { alertActions } from '../_actions'
@@ -24,22 +25,20 @@ class App extends Component {
   render() {
     const { alert } = this.props;
     return (
-      <div className="jumbotron">
-          <div className="container">
-              <div className="col-sm-8 col-sm-offset-2">
-                  {alert.message &&
-                      <div className={`alert ${alert.type}`}>{alert.message}</div>
-                  }
-                  <Router history={history}>
-                      <div>
-                          <PrivateRoute exact path="/" component={HomePage} />
-                          <Route path="/login" component={LoginPage} />
-                          <Route path="/register" component={RegisterPage} />
-                      </div>
-                  </Router>
-              </div>
-          </div>
-      </div>
+        <div className="app">
+            <Container>
+                {alert.message &&
+                    <div className={`alert ${alert.type}`}>{alert.message}</div>
+                }
+                <Router history={history}>
+                    <div>
+                        <PrivateRoute exact path="/" component={HomePage} />
+                        <Route path="/login" component={LoginPage} />
+                        <Route path="/register" component={RegisterPage} />
+                    </div>
+                </Router>
+            </Container>
+        </div>
     );
   }
 }
