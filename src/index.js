f7b54d1f6dffafa660ca80d208a1c5e550d91709
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2019-01-24 13:48:45 +0100

    Add reactstrap to use bootstrap 4

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { store } from './_helpers';
 import { App } from './App';
