4a2255b988d6e4ccdc71b683857e3d831fda271e
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2019-01-28 13:03:38 +0100

    Add propTypes and defaultProps to ButtonLink component in Sign

--- src/_scenes/Sign/components/ButtonLink.jsx
+++ src/_scenes/Sign/components/ButtonLink.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import cx from 'classnames';
 import styles from '../sign_shared.module.css'
+
+ButtonLink.propTypes = {
+  link: PropTypes.string.required,
+  linkText: PropTypes.string.required
+}
+
+ButtonLink.defaultProps = {
+  link: '',
+  linkText: ''
+}
 
 function ButtonLink({link, linkText}) {  
   return (
