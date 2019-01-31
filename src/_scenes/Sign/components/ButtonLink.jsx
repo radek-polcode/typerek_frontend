5f755ceacf2c828aefbebc2668975fd27306f590
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2019-01-31 11:38:27 +0100

    Move styles for forms and buttons to global App.css

--- src/_scenes/Sign/components/ButtonLink.jsx
+++ src/_scenes/Sign/components/ButtonLink.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-import cx from 'classnames';
-import styles from '../sign_shared.module.css'
+import '../../../App/App.css'
 
 ButtonLink.propTypes = {
   link: PropTypes.string.required,
@@ -19,7 +18,7 @@ function ButtonLink({link, linkText}) {
   return (
     <>
       <Link to={link} 
-            className={cx("btn", styles.card__auth__btn__link)}>
+            className="btn card__btn__link">
         {linkText}
       </Link>
     </>
