2f08ba2ab0655b51275a485c55a980c0721836a8
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2019-02-13 15:50:10 +0100

    Remove redundant functions

--- src/_services/authentication.service.js
+++ src/_services/authentication.service.js
@@ -1,3 +1,4 @@
+import { handlingResponse } from '../_helpers';
 import config from '../_config';
 
 export const authenticationService = {
@@ -5,6 +6,8 @@ export const authenticationService = {
   logout,
   register,
 };
+
+const handleResponse = handlingResponse.handleResponse
 
 function login(email, password) {
   const requestOptions = {
@@ -32,41 +35,4 @@ function register(user) {
   }
 
   return fetch(`${config.apiUrl}/auth`, requestOptions).then(handleResponse)
-}
-
-function handleResponse(response) {
-  return response.text().then(text => {
-      let data = text && JSON.parse(text);
-      let userFromLocalStorage = localStorage.getItem('user')
-      // set auth headers if user not exists in localStorage 
-      if (data 
-          && response.headers.get('access-token') 
-          && userFromLocalStorage == undefined) {
-        data = assignAuthHeaders(data, response.headers)
-      }
-      if (!response.ok) {
-          if (response.status === 401) {
-              // auto logout if 401 response returned from api
-              logout();
-              // window.location.reload(true);
-          }
-
-          const error = (data && data.message) || response.statusText;
-          return Promise.reject(error);
-      }
-      return data;
-  });
-}
-
-function assignAuthHeaders(data, responseHeaders) {
-  return Object.assign(
-          { 
-            'authData': {
-              'access-token': responseHeaders.get('access-token'),
-              'client': responseHeaders.get('client'),
-              'expiry': responseHeaders.get('expiry'),
-              'token-type': responseHeaders.get('token-type'),
-              'uid': responseHeaders.get('uid')
-            }
-          }, data)
 }
--- src/_services/competition.service.js
+++ src/_services/competition.service.js
@@ -1,4 +1,4 @@
-import { authenticationHeaders } from '../_helpers';
+import { authenticationHeaders, handlingResponse } from '../_helpers';
 import config from '../_config';
 
 const namespace = 'admin/'
@@ -7,10 +7,7 @@ export const competitionService = {
   getAll
 };
 
-
-function logout() {
-  localStorage.removeItem('user');
-}
+const handleResponse = handlingResponse.handleResponse
 
 function getAll() {
   const requestOptions = {
@@ -20,41 +17,4 @@ function getAll() {
 
   return fetch(`${config.apiUrl}/${namespace}competitions`, requestOptions)
           .then(handleResponse);
-}
-
-function handleResponse(response) {
-  return response.text().then(text => {
-      let data = text && JSON.parse(text);
-      let userFromLocalStorage = localStorage.getItem('user')
-      // set auth headers if user not exists in localStorage 
-      if (data 
-          && response.headers.get('access-token') 
-          && userFromLocalStorage == undefined) {
-        data = assignAuthHeaders(data, response.headers)
-      }
-      if (!response.ok) {
-          if (response.status === 401) {
-              // auto logout if 401 response returned from api
-              logout();
-              // window.location.reload(true);
-          }
-
-          const error = (data && data.message) || response.statusText;
-          return Promise.reject(error);
-      }
-      return data;
-  });
-}
-
-function assignAuthHeaders(data, responseHeaders) {
-  return Object.assign(
-          { 
-            'authData': {
-              'access-token': responseHeaders.get('access-token'),
-              'client': responseHeaders.get('client'),
-              'expiry': responseHeaders.get('expiry'),
-              'token-type': responseHeaders.get('token-type'),
-              'uid': responseHeaders.get('uid')
-            }
-          }, data)
 }
