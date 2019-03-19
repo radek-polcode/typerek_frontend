933df34216bcd3d23c7a7ba5479207ca305a2422
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2019-03-19 16:32:58 +0100

    Remove UsersTable component

--- src/_scenes/Admin/scenes/Users/Users.jsx
+++ src/_scenes/Admin/scenes/Users/Users.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Button, Card, CardHeader, CardBody, Table } from 'reactstrap';
 import { withNamespaces } from 'react-i18next';
 
 import { alertActions } from '../../../../_actions/alert.actions'
 import { modalActions } from '../../../../_actions/modal.actions'
 import { userActions } from '../../../../_actions/user.actions'
 
-import { UsersTable } from './UsersTable'
+import { UsersTableRow } from './UsersTableRow'
 
 class Users extends Component {
   static propTypes = {
@@ -33,11 +33,41 @@ class Users extends Component {
 
     return (
       <div>
-          <Link to='/admin/users/new'>{t('admin.usersTable.addNewUser')}</Link>
-          <UsersTable 
-            users={users}
-            handleDeleteUser={handleDeleteUser}
-          />
+        <Button> 
+          {t('admin.usersTable.addNewUser')}
+        </Button>
+        <Card className="card__form">
+          <CardHeader tag="h2">
+            {t('admin.usersTable.title')}
+          </CardHeader>
+          <CardBody>
+            <Table responsive
+              >
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>{t('shared.username')}</th>
+                    <th>{t('shared.email')}</th>
+                    <th>{t('shared.role')}</th>
+                    <th>{t('shared.takesPart')}</th>
+                    <th>{t('admin.usersTable.registered')}</th>
+                    <th>{t('shared.action')}</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {users.items &&
+                    users.items.map((user, index) =>
+                    <UsersTableRow
+                      key={user.id}
+                      handleDeleteUser={handleDeleteUser}
+                      index={index}
+                      user={user}
+                    />
+                  )}
+                </tbody>
+            </Table>
+          </CardBody>
+        </Card>
       </div>
     )
   }
